fb2f249dcabdeefba886a395fb8cbed6342e933f
sjoon-oh <sjoon.pm@gmail.com>
2022-03-29 17:35:43 +0900

    Fix 1

--- hw3/hw3-d-2.py
+++ hw3/hw3-d-2.py
@@ -8,7 +8,16 @@ BITMASK = 0xffffffffffffffff # 64 bit
 # BITMASK = 0xffffffffffffffffffffffffffffffff # 128 bit
 
 INITIAL_X = 20223402
-ELEM_THRESH = 0xffffffffffff
+
+# Test.
+LOOP_THRESH = 0xfffffffff # At single loop, how many files?
+FILE_THRESH = 0xffffff
+
+
+
+
+
+
 
 tracemalloc.start()
 
@@ -24,95 +33,131 @@ def do_hash(val):
     return \
         int(hashlib.md5(val.to_bytes(16, 'big', signed=True)).hexdigest(), 16) & BITMASK
 
-hist = {}
-file_cnt = 0
-next_h = INITIAL_X
 
-#
-# First round: Generate Database
-print("\nRunning first round...")
-for i in range(1, BITMASK + 1):
-    
-    next_h = do_hash(next_h)
 
-    try:
-        val = hist[next_h] # Test
-
-        with open(f"report-hw3-d-2.log", 'w') as f:
-            f.write(f"Found: Counts - {i}\n")
-
-        print(f"\nFound: Hash: {next_h}, Count {i}\n")
-        exit()
-
-    except KeyError:
-        hist[next_h] = ''
-
+def run_first_round(next_h, file_cnt):
     #
-    # Save to JSON
-    if (i % ELEM_THRESH == 0):
-        with open(f"./hw3-d-json/hist-{file_cnt}.json", 'w') as outfile:
-            json.dump(hist, outfile, sort_keys=False)
+    # First round: Generate Database
+    print("Running first round...")
 
-        del hist
+    hist = {}
 
-        hist = {}
-        file_cnt = file_cnt + 1
+    for i in range(0, LOOP_THRESH):
+        
+        next_h = do_hash(next_h)
 
-    mem = tracemalloc.get_traced_memory()
-    print(f"\rHashing: {i: {10}} - {'%4.2f' % (i / BITMASK * 100)}%, Memory: {'%6.3f' % (mem[0] / 10**6)}MB, File Written ID: {file_cnt - 1: {10}}", end="")
+        try:
+            val = hist[next_h] # Test
+
+            with open(f"report-hw3-d-2.log", 'w') as f:
+                f.write(f"Found at first round: Counts - {i}\n")
+
+            print(f"\nFound: Hash: {next_h}, Count {i}\n")
+            exit()
+
+        except KeyError:
+            hist[next_h] = ''
+
+        #
+        # Save to JSON
+        if (i % FILE_THRESH == 0):
+            with open(f"./hw3-d-json/hist-{file_cnt}.json", 'w') as outfile:
+                json.dump(hist, outfile, sort_keys=False)
+
+            del hist
+
+            hist = {}
+            file_cnt = file_cnt + 1
+
+        mem = tracemalloc.get_traced_memory()
+        print(f"\rHashing: {i: {10}} - {'%4.2f' % (i / BITMASK * 100)}%, Memory: {'%6.3f' % (mem[0] / 10**6)}MB, File Written ID: {file_cnt - 1: {10}}", end="")
+
+    
+    file_cnt = file_cnt - 1 # Current latest. Not to-be-written
+    return next_h, file_cnt
         
 
+def run_second_round(test_file_range):
+    #
+    # Second round:
+    print("\nRunning second round...")
+
+    if (test_file_range < 1): 
+        print("Not enough JSON files. ERROR.")
+        exit()
+
+    else:
+        idx_base = FILE_THRESH
+
+        for i in range(1, test_file_range):
+
+            target_keys = 0
+            with open(f"./hw3-d-json/hist-{i}.json", "r") as f:
+
+                target_keys = json.load(f)
+                target_keys = [int(_) for _ in target_keys.keys()]
+
+            for j in range(i): # vs files.
+                
+                #
+                # Recover vs files  
+                hist = {}
+                with open(f"./hw3-d-json/hist-{j}.json", "r") as f:
+
+                    hist = json.load(f)
+                    int_keys = [int(_) for _ in hist.keys()]
+
+
+                    del hist
+                    hist = {}
+
+                    for _ in int_keys: hist[_] = ''
+                
+                mem = tracemalloc.get_traced_memory()
+                print(f"\rChecking: {i: {6}} vs {j: {6}} file ID, {'%4.2f' % (i / BITMASK * 100)}%, Memory: {'%6.3f' % (mem[0] / 10**6)}MB", end="")
+
+                for k, target in enumerate(target_keys):
+                    try:
+                        val = hist[target]
+
+                        with open(f"report-hw3-d-2.log", 'w') as f:
+                            f.write(f"Found: Counts - {i}\n")
+
+                            f.write(f"\nFound at second round: {target},\n")
+                            f.write(f"  Each file size: {FILE_THRESH}\n")
+                            f.write(f"  Current position file: {i}, vs file {j}, of offset {k + 1}\n")
+                            f.write(f"  Total index position: {1 + k + idx_base}\n")
+                        exit()
+
+                    except KeyError:
+                        continue
+
+            idx_base = idx_base + len(target_keys)
+
+
 
 #
-# Second round:
-print("\nRunning second round...")
+# Main script run.
 
-if (file_cnt < 1): 
-    print("Not enough JSON files. ERROR.")
-    exit()
+hist = {}
 
-else:
-    idx_base = ELEM_THRESH
+next_h = INITIAL_X
 
-    for i in range(1, file_cnt):
+file_cnt = 0
+loop_cnt = 0
 
-        target_keys = 0
-        with open(f"./hw3-d-json/hist-{i}.json", "r") as f:
-
-            target_keys = json.load(f)
-            target_keys = [int(_) for _ in target_keys.keys()]
-
-        for j in range(i): # vs files.
-            
-            #
-            # Recover vs files  
-            hist = {}
-            with open(f"./hw3-d-json/hist-{j}.json", "r") as f:
-
-                hist = json.load(f)
-                int_keys = [int(_) for _ in hist.keys()]
+while (loop_cnt < BITMASK):
 
 
-                del hist
-                hist = {}
+    next_h, file_cnt = run_first_round(next_h, file_cnt)
 
-                for _ in int_keys: hist[_] = ''
-            
-            mem = tracemalloc.get_traced_memory()
-            print(f"\rChecking: {i: {6}} vs {j: {6}} file ID, {'%4.2f' % (i / BITMASK * 100)}%, Memory: {'%6.3f' % (mem[0] / 10**6)}MB", end="")
+    run_second_round(file_cnt)
 
-            for k, target in enumerate(target_keys):
-                try:
-                    val = hist[target]
-                    print(f"\nFound: {target},")
-                    print(f"  Each file size: {ELEM_THRESH}")
-                    print(f"  Current position file: {i}, vs file {j}, of offset {k + 1}")
-                    print(f"  Total index position: {1 + k + idx_base}")
-                    exit()
+    file_cnt = file_cnt + 1
+    loop_cnt = loop_cnt + 1
 
-                except KeyError:
-                    continue
+    print(f"Checkpoint: (loop count: {loop_cnt}, {file_cnt})")
 
-        idx_base = idx_base + len(target_keys)
+print("Something's wrong, nothing found.")
 
-    tracemalloc.stop()
+tracemalloc.stop()
