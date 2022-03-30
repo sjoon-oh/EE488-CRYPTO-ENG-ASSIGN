2d0be98a4de5e265777fc31165dbe6a9201d08cb
sjoon-oh <sjoon.pm@gmail.com>
2022-03-30 21:17:18 +0900

    Major fix

--- hw3/hw3-d-2.py
+++ hw3/hw3-d-2.py
@@ -3,14 +3,14 @@ import hashlib
 
 import tracemalloc
 
-# BITMASK = 0xffffffffffffffff # 64 bit
-BITMASK = 0xffffffffffffffffffff # 80 bit
+BITMASK = 0xffffffffffffffff # 64 bit
+# BITMASK = 0xffffffffffffffffffff # 80 bit
 # BITMASK = 0xffffffffffffffffffffffffffffffff # 128 bit
 
 INITIAL_X = 20223402
 
 # Test.
-LOOP_THRESH = 0xffffffff # At single loop, how many files?
+LOOP_THRESH = 0xffffffffff # At single loop, how many files?
 FILE_THRESH = 0xffffff
 
 
@@ -38,7 +38,7 @@ def run_first_round(next_h, file_cnt):
     hist = {}
     prev_h = next_h
 
-    for i in range(0, LOOP_THRESH):
+    for i in range(0, FILE_THRESH):
         
         prev_h = next_h
         next_h = do_hash(prev_h)
@@ -55,22 +55,20 @@ def run_first_round(next_h, file_cnt):
         except KeyError:
             hist[next_h] = ''
 
-        #
-        # Save to JSON
-        if (i % FILE_THRESH == 0):
-            with open(f"./hw3-d-json/hist-{file_cnt}.json", 'w') as outfile:
-                json.dump(hist, outfile, sort_keys=False)
 
-            del hist
-
-            hist = {}
-            file_cnt = file_cnt + 1
 
         mem = tracemalloc.get_traced_memory()
-        print(f"\rHashing: {i: {10}} - {'%4.2f' % (i / LOOP_THRESH * 100)}% of threshold, Memory: {'%6.3f' % (mem[0] / 10**6)}MB, File Written ID: {file_cnt - 1: {10}}", end="")
+        print(f"\rHashing: {i: {10}} - {'%4.2f' % (i / FILE_THRESH * 100)}% of threshold, Memory: {'%8.2f' % (mem[0] / 10**6)}MB, File Written ID: {file_cnt: {10}}", end="")
 
-    
-    file_cnt = file_cnt - 1 # Current latest. Not to-be-written
+    #
+    # Save to JSON
+
+    with open(f"./hw3-d-json/hist-{file_cnt}.json", 'w') as outfile:
+        json.dump(hist, outfile, sort_keys=False)
+
+    del hist
+    file_cnt = file_cnt + 1
+
     return next_h, file_cnt
         
 
@@ -80,8 +78,8 @@ def run_second_round(test_file_range):
     print("\nRunning second round...")
 
     if (test_file_range < 1): 
-        print("Not enough JSON files. ERROR.")
-        exit()
+        print("Not enough JSON files. Jump.")
+        return
 
     else:
         idx_base = FILE_THRESH
@@ -111,7 +109,7 @@ def run_second_round(test_file_range):
                     for _ in int_keys: hist[_] = ''
                 
                 mem = tracemalloc.get_traced_memory()
-                print(f"\rChecking: {i: {6}} vs {j: {6}} file ID, {'%4.2f' % (i / FILE_THRESH * 100)}%, Memory: {'%6.3f' % (mem[0] / 10**6)}MB", end="")
+                print(f"\rChecking: {i: {6}} vs {j: {6}} file ID, {'%8.2f' % (i / FILE_THRESH * 100)}%, Memory: {'%8.2f' % (mem[0] / 10**6)}MB", end="")
 
                 for k, target in enumerate(target_keys):
                     try:
@@ -129,9 +127,11 @@ def run_second_round(test_file_range):
 
                     except KeyError:
                         continue
+                
+                del hist
 
             idx_base = idx_base + len(target_keys)
-
+            del target_keys
 
 
 #
@@ -141,20 +141,18 @@ hist = {}
 
 next_h = INITIAL_X
 
-file_cnt = 0
+file_cnt = -1
 loop_cnt = 0
 
-while (loop_cnt < BITMASK):
+while (loop_cnt < (BITMASK // FILE_THRESH)):
 
 
     next_h, file_cnt = run_first_round(next_h, file_cnt)
 
     run_second_round(file_cnt)
-
-    file_cnt = file_cnt + 1
     loop_cnt = loop_cnt + 1
 
-    print(f"Checkpoint: (loop count: {loop_cnt}, {file_cnt})")
+    print(f"\nCheckpoint: (loop count: {loop_cnt}, {file_cnt})")
 
 print("Something's wrong, nothing found.")
 
